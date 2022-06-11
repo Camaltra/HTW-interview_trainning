6032028bc09efb29aec23eab3ae5aafea7d30220
Camaltra <mickael.boillaud@gmail.com>
2022-06-11 17:28:10 +0200

    Adding the first task of the section0

---
+++ 0x00-array_string/0-two_sum.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def twoSum(self, nums, target):
+    dictOfNum = {}
+    for i, value in enumerate(nums):
+        diff = target - value
+        if diff in dictOfNum:
+            return [i, dictOfNum[diff]]
+        dictOfNum[nums[i]] = i
