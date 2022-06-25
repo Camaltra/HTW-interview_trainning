fd2e21f98a64b6ea2b3fd00f69f193ec97e14435
Camaltra <mickael.boillaud@gmail.com>
2022-06-25 15:50:46 +0200

    Adding the task 3

---
+++ 0x00-array_string/2-max_profit.py
@@ -0,0 +1,12 @@
+def maxProfitChecker(prices):
+    if not prices:
+        return 0
+    heap = []
+    maxAllTime = 0
+    heap.append(prices[0])
+    for i in range(1, len(prices)):
+        while(heap and heap[-1] > prices[i]):
+            heap.pop(-1)
+        heap.append(prices[i])
+        maxAllTime = max(heap[-1] - heap[0], maxAllTime)
+    return maxAllTime
