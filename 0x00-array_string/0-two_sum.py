9a366aad62ffb30057b7e6e2c83e1c06d883b747
Camaltra <mickael.boillaud@gmail.com>
2022-06-27 10:15:54 +0200

    Introduce a syntaxe error

--- 0x00-array_string/0-two_sum.py
+++ 0x00-array_string/0-two_sum.py
@@ -2,7 +2,7 @@
 
 def twoSum(nums, target):
     dictOfNum = {}
-    for i, value in enumerate(nums):
+    for i, value in enumerate(nums)
         diff = target - value
         if diff in dictOfNum:
             return [i, dictOfNum[diff]]
