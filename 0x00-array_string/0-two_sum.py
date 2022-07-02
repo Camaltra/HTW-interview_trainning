2e1d1abc138b2d5efede6782c5b5c1c37cdf02bc
Camaltra <mickael.boillaud@gmail.com>
2022-07-02 23:52:00 +0200

    reverse

--- 0x00-array_string/0-two_sum.py
+++ 0x00-array_string/0-two_sum.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
 def twoSum(nums, target):
-    return [2, 1]
+    dictOfNum = {}
+    for i, value in enumerate(nums):
+        diff = target - value
+        if diff in dictOfNum:
+            return [i, dictOfNum[diff]]
+        dictOfNum[nums[i]] = i
